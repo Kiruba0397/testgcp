48bc2ab1e7fd4f92503b28d1259a29fb87da1cb5
Kiruba0397 <56583046+celinwincy0310@users.noreply.github.com>
2022-03-21 17:55:14 +0530

    gcp

--- src/main/java/com/example/gcp/GcpApplication.java
+++ src/main/java/com/example/gcp/GcpApplication.java
@@ -1,4 +1,4 @@
-package com.example.Teamazure.Teamazure;
+package com.example.gcp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,14 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class TeamazureApplication {
+public class GcpApplication {
 	@GetMapping("/gcp")
 	public String message() {
 		return "GCP App Engine Service - Build & Deployed Successfully using GCP Pipeline";
 	}
 	
 	public static void main(String[] args) {
-		SpringApplication.run(TeamazureApplication.class, args);
+		SpringApplication.run(GcpApplication.class, args);
 	}
 
 }
